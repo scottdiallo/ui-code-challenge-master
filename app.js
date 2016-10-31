811f93b0a4474a8e4a075c5474aa3cc05a658eb2
Scott Diallo <diallot@me.com>
2016-10-30 17:49:12 -0700

    hover effect applied to the arrow

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 $(function() {
      console.log( "ready!" );
+     $( ".arrowDown" ).hover(function() {
+         background-color(color:red);
+    //    $( this ).fadeOut( 100 );
+    //    $( this ).fadeIn( 500 );
+     });
 
-     $(window).scroll(function() {
-     		$('.bg-img').each(function(){
-     		var imagePos = $(this).offset().top;
+    //  $(window).scroll(function() {
+    //  		$('.bg-img').each(function(){
+    //  		var imagePos = $(this).offset().top;
+     //
+    //  		var topOfWindow = $(window).scrollTop();
+    //  			if (imagePos < topOfWindow+400) {
+    //  				$(this).addClass("bg-img");
+    //  			}
+    //  		});
+    //  	});
 
-     		var topOfWindow = $(window).scrollTop();
-     			if (imagePos < topOfWindow+400) {
-     				$(this).addClass("bg-img");
-     			}
-     		});
-     	});
 
 });
