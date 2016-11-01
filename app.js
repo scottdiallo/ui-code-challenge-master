670f190610e0c87fd6a49d4e624e0bedd081bd28
Scott Diallo <diallot@me.com>
2016-11-01 16:07:21 -0700

    minor changes

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 $(function() {
-     console.log( "ready!" );
-     $( ".arrowDown" ).hover(function() {
-         background-color(color:red);
-    //    $( this ).fadeOut( 100 );
-    //    $( this ).fadeIn( 500 );
-     });
+    //  console.log( "ready!" );
+    //  $( ".arrowDown" ).hover(function() {
+    //      background-color(color:red);
+    // //    $( this ).fadeOut( 100 );
+    // //    $( this ).fadeIn( 500 );
+    //  });
 
-    //  $(window).scroll(function() {
-    //  		$('.bg-img').each(function(){
-    //  		var imagePos = $(this).offset().top;
-     //
-    //  		var topOfWindow = $(window).scrollTop();
-    //  			if (imagePos < topOfWindow+400) {
-    //  				$(this).addClass("bg-img");
-    //  			}
-    //  		});
-    //  	});
+     $(window).scroll(function() {
+     		$('.bg-img').each(function(){
+     		var imagePos = $(this).offset().top;
+
+     		var topOfWindow = $(window).scrollTop();
+     			if (imagePos < topOfWindow+400) {
+     				$(this).addClass("bg-img");
+     			}
+     		});
+     	});
 
 
 });
