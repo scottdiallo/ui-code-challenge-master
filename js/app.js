4e46ffe79d79a6092517808b670914ad2d2e976b
Scott Diallo <diallot@me.com>
2016-11-04 17:43:04 -0700

    smooth scrolling applied to the down arrow

--- js/app.js
+++ js/app.js
@@ -1,18 +1,62 @@
-/*--------------------------------------------
- Smooth Scrolling Anchors
----------------------------------------------*/
+$(document).ready(function(){
+    console.log("hello world");
+    //  Smooth Scrolling Anchors
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+     $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 
-$(document).ready(function() {
-    // console.log('ready');
- $(".arrowDown").click(function(e) {
-   e.preventDefault();
-   $('.bg-img').hide();
-   $('html,body').animate({scrollTop:$(this.hash).offset().top + 1}, 1000, function() {
-     $('.image2').show();
-   });
- });
-
- // applying parallax effect to the 1st image
- $('.parallax-window').parallax({imageSrc: 'image1.jpg'});
-
+  
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ---------------------------------------------*/
+//
+// $(document).ready(function() {
+//     console.log("hello world");
+//  // $(".arrowDown").click(function(e) {
+//  //   e.preventDefault();
+//  //   $('.bg-img').hide();
+//  //   $('html,body').animate({scrollTop:$(this.hash).offset().top + 1}, 1000, function() {
+//  //     $('.image2').show();
+//  //   });
+//  // });
+//
+//  // applying parallax effect to the 1st image
+// //  $('.parallax-window').parallax({imageSrc: 'image1.jpg'});
+// //
+// // applying smooth scrolling to the arrow
+// $('a[href*="#"]:not([href="#"])').click(function() {
+//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//       var target = $(this.hash);
+//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//       if (target.length) {
+//         $('html, body').animate({
+//           scrollTop: target.offset().top
+//         }, 1000);
+//         return false;
+//       }
+//     }
+//   });
+//
+//
+// });
